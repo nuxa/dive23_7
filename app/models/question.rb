fd9d3961f8848e3f35977646e5b8603910c5b7ad
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2017-02-05 22:19:12 +0900

    Questionモデルのvalidation付加 issues/#1

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ActiveRecord::Base
   has_many :favorite
   has_many :answer
   has_many :points, class_name: 'Contribute'
+
+  validates :title,   presence: true, length: { maximum: 50 }
+  validates :content, presence: true
+
 end
