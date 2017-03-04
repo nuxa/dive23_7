27bddc75ed8fa189f1e8b952e124ac34b7d86f3a
hiramatch <hiramatch@gmail.com>
2017-03-04 20:11:19 +0900

    【その他】ログイン画面に遷移できない事象の対応 issues/#43

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  
-  resources :users, only: [:index, :show] do
-    resources :favorites, only: [:index]
-  end
 
   devise_for :users, controllers: {
       registrations: 'users/registrations',
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
-
+  resources :users, only: [:index, :show] do
+    resources :favorites, only: [:index]
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
