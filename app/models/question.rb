774d8c6b39db7581a06a11ab3af0b503923a2c62
伊藤 <rhcp2106@fork.ocn.ne.jp>
2017-04-09 20:31:43 +0900

    【共通】Viewの微調整 #34

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  has_many :answers
+  has_many :answers, dependent: :destroy
   has_many :q_points, class_name: 'Contribute', foreign_key: :point_q
 
   validates :title,   presence: true, length: { maximum: 50 }
