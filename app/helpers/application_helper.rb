1590aa4c51a4168d111bad493f5ad6668b592836
itottm <itottm@users.noreply.github.com>
2017-04-15 16:11:30 +0900

    Revert "【ユーザー機能】マイページで回答が表示されていない事象の改修およびデザイン修正 issues/#77"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name, size: "200x200") if user.avatar?
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
 
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name, size: "200x200")
+    image_tag(img_url, alt: user.name)
   end
 
   def switch_active_class(c_name, a_name='')
